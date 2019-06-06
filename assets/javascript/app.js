3c4b73b989ccdd08f3e1a24104f6894cfdfea152
emilybaum <emily.r.baum@gmail.com>
2019-06-06 19:11:21 -0400

    ajax request is working for pixabay with the interest being predefined (in the button for testing)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,36 @@
 // javascript
+
+var userInterest = "flowers"
+
+// getImagesPixabay(userInterest)
+
+$("#TBDforButton").on("click", function() {
+    var interest = $(this).attr("interest"); // do we need to concatenate this if multiple words?
+    var API = "12697501-1309c320d0a4f2a4386273ea4";
+    var queryURL = "https://pixabay.com/api/?key=" + API + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
+    // possibilities of other restrictions to add
+    // editors_choice
+    // orientation (if we need horizontal vs vertical)
+    // min_width
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var numOfImages = 4;
+
+        for (var i = 0; i < numOfImages; i++) {
+            var pixabayDiv = $("<div>")
+            var image = response.hits[i].webformatURL;
+            console.log(image)
+            var disaplyImage = $("<img>");
+            disaplyImage.attr("src", image)
+            pixabayDiv.append(disaplyImage)
+            $("#TBDforWhere").append(pixabayDiv);
+        }
+        
+    })
+
+
+})
