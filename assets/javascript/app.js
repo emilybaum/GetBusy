3912a7d27aad06688f6bb2918972d04ea1df46e0
emilybaum <emily.r.baum@gmail.com>
2019-06-06 22:21:04 -0400

    functioning userData object from input form and commented out all the pixabay ajax as it;s still WIP

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,81 +9,60 @@ var userData = {
 }
 console.log(userData)
 
-// var pixArrIndex = 0
+var pixArrIndex = 0
 
-// button clikc at end of form
+// button click at end of form
 $("#TBDforButton").on("click", collectUserData)
 
 // function to collect the data that is added by the user and feeds into the object
 function collectUserData() {
+    event.preventDefault()
     userData.userName = $("#userName-input").val().trim()
     userData.email = $("#email-input").val().trim()
     userData.postalCode = $("#postalCode-input").val().trim()
     userData.interest = $("#interest-input").val().trim()
     console.log(userData)
-    getImagesPixabay(userData)
+    // getDataPixabay(userData)
 }
 
 
+
+// var newImagesArr = [];
+
+// // $("#TBDforButton").on("click", getDataPixabay)
+
+// $("#loadMorePix").on("click", displayPixabay)
+
+// function displayPixabay(arr) {
+//     // event.preventDefault()
+
+//     var numOfImages = 4
+//     for (var i = 0; i < numOfImages; i++) {
+//         var pixabayDiv = $("<div>")
+//         var image = hits[i].webformatURL;
+
+//         var disaplyImage = $("<img>");
+//         disaplyImage.attr("src", image)
+//         pixabayDiv.append(disaplyImage)
+//         $("#TBDforWhere").append(pixabayDiv);
+//     }
+//     arr.splice(0, 4);
+//     newImagesArr = arr;
+//     console.log(newImagesArr);
+// }
     
-// $("#TBDforButton").on("click", getImagesPixabay)
-
-function getImagesPixabay(userData) {
-    event.preventDefault()
-    var interest = userData.interest; // do we need to concatenate this if multiple words?
-    console.log("this is the user's Interest " + userData.interest)
-    var API = "12697501-1309c320d0a4f2a4386273ea4";
-    var queryURL = "https://pixabay.com/api/?key=" + API + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-
-        var numOfImages = 4
-
-        for (var i = 0; i < numOfImages; i++) {
-            var pixabayDiv = $("<div>")
-            var image = response.hits[i].webformatURL;
-            console.log("this is the image variable " + image)
-
-            var disaplyImage = $("<img>");
-            disaplyImage.attr("src", image)
-            pixabayDiv.append(disaplyImage)
-            $("#TBDforWhere").append(pixabayDiv);
-        }
-        // pixArrIndex += 4;
-    })
-}
-
-// $("#loadMorePix").on("click", loadMorePixabay)
-
-// function loadMorePixabay(userData) {
+// var interest = userData.interest; // do we need to concatenate this if multiple words?
+// var APIImages = "12697501-1309c320d0a4f2a4386273ea4";
+// var queryURLImages = "https://pixabay.com/api/?key=" + APIImages + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
+    
+// function getDataPixabay() {
 //     event.preventDefault()
-//     var interest = userData.interest; // do we need to concatenate this if multiple words?
-//     var API = "12697501-1309c320d0a4f2a4386273ea4";
-//     var queryURL = "https://pixabay.com/api/?key=" + API + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
-
 //     $.ajax({
-//         url: queryURL,
+//         url: queryURLImages,
 //         method: "GET"
-//     }).then(function (response) {
-//         // $("#TBDforWhere").empty()
+//     }).then(function(response) {
 //         console.log(response);
-
-//         var numOfImages = 4
-
-//         for (var i = pixArrIndex; i < numOfImages; i++) {
-//             var pixabayDiv = $("<div>")
-//             var image = response.hits[i].webformatURL;
-//             console.log(image)
-
-//             var disaplyImage = $("<img>");
-//             disaplyImage.attr("src", image)
-//             pixabayDiv.append(disaplyImage)
-//             $("#TBDforWhere").append(pixabayDiv);
-//         }
-//         pixArrIndex + 4;
+//         var newImagesArr = response.hits
+//         displayPixabay(newImagesArr);
 //     })
 // }
