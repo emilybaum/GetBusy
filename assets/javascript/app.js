3f7a307b975dc8d53c05d4de7aa2226e0e1710c3
emilybaum <emily.r.baum@gmail.com>
2019-06-06 21:41:11 -0400

    fixed the userData option and its working again - move the generate Pixabay function into the collect user data function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,9 @@ var userData = {
     postalCode: 0,
     interest: "",
 }
+console.log(userData)
+
+// var pixArrIndex = 0
 
 // button clikc at end of form
 $("#TBDforButton").on("click", collectUserData)
@@ -18,42 +21,69 @@ function collectUserData() {
     userData.postalCode = $("#postalCode-input").val().trim()
     userData.interest = $("#interest-input").val().trim()
     console.log(userData)
+    getImagesPixabay(userData)
 }
 
 
-// capture interest from form submission and feed into global variables
     
-$("#TBDforButton").on("click", getImagesPixabay)
+// $("#TBDforButton").on("click", getImagesPixabay)
 
 function getImagesPixabay(userData) {
     event.preventDefault()
     var interest = userData.interest; // do we need to concatenate this if multiple words?
+    console.log("this is the user's Interest " + userData.interest)
     var API = "12697501-1309c320d0a4f2a4386273ea4";
     var queryURL = "https://pixabay.com/api/?key=" + API + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
-    // possibilities of other restrictions to add
-    // editors_choice
-    // orientation (if we need horizontal vs vertical)
-    // min_width
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
-        var numOfImages = 20;
-        $("#TBDforWhere").empty()
+
+        var numOfImages = 4
 
         for (var i = 0; i < numOfImages; i++) {
             var pixabayDiv = $("<div>")
             var image = response.hits[i].webformatURL;
-            console.log(image)
+            console.log("this is the image variable " + image)
+
             var disaplyImage = $("<img>");
             disaplyImage.attr("src", image)
             pixabayDiv.append(disaplyImage)
             $("#TBDforWhere").append(pixabayDiv);
         }
-        
+        // pixArrIndex += 4;
     })
-
-
 }
+
+// $("#loadMorePix").on("click", loadMorePixabay)
+
+// function loadMorePixabay(userData) {
+//     event.preventDefault()
+//     var interest = userData.interest; // do we need to concatenate this if multiple words?
+//     var API = "12697501-1309c320d0a4f2a4386273ea4";
+//     var queryURL = "https://pixabay.com/api/?key=" + API + "&q=" + interest + "&image_type=photo" + "&editors_choice=true" + "&page=1";
+
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         // $("#TBDforWhere").empty()
+//         console.log(response);
+
+//         var numOfImages = 4
+
+//         for (var i = pixArrIndex; i < numOfImages; i++) {
+//             var pixabayDiv = $("<div>")
+//             var image = response.hits[i].webformatURL;
+//             console.log(image)
+
+//             var disaplyImage = $("<img>");
+//             disaplyImage.attr("src", image)
+//             pixabayDiv.append(disaplyImage)
+//             $("#TBDforWhere").append(pixabayDiv);
+//         }
+//         pixArrIndex + 4;
+//     })
+// }
