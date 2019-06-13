abb3f944a8a2bf6d433fa21b8bea2f5042fdda4a
emilybaum <emily.r.baum@gmail.com>
2019-06-13 00:57:17 -0400

    added some structure to the invite page along with animation library to use

--- assets/javascript/app-invite.js
+++ assets/javascript/app-invite.js
@@ -6,12 +6,34 @@ function renderInvite() {
     console.log(userData);
     inviteObject = JSON.parse(localStorage.getItem("inviteObject"));
     console.log(inviteObject);
-    $("#invite-line").text(userData.userName + ", will you go out with me?")
-    $("#pixabay-display").append("<img src=" + inviteObject.pixabay.dataImg + ">")
-    $("#etsy-display").append("<img src=" + inviteObject.etsy.dataImg + ">")
-    $("#etsy-display").append("<h5>" + inviteObject.etsy.dataTitle + "</h5>")
-    $("#eventbrite-display").append("<img src=" + inviteObject.eventbrite.dataImg + ">")
-    $("#eventbrite-display").append("<h5>" + inviteObject.eventbrite.dataName + "</h5>")
+
+    // user name
+    var userNameInvite = userData.userName 
+    $("#invite-line").text(userNameInvite)
+
+    // pixabay
+    var pixabayInviteImg = ("<img class='invite-img' src=" + inviteObject.pixabay.dataImg + ">")
+    $("#pixabay-display").prepend(pixabayInviteImg)
+    var userInterestInvite = userData.interest
+    $("#userInterest-invite").text(userInterestInvite)
+
+    // etsy
+    var etsyInviteImg = ("<img class='invite-img' src=" + inviteObject.etsy.dataImg + ">")
+    $("#etsy-display").append(etsyInviteImg)
+    etsyInviteDetails1 = ("<h5 class='invite-card-text'>" + inviteObject.etsy.dataTitle + "</h5>")
+    $("#etsy-display").append(etsyInviteDetails1)
+    // add link to item
+    // add cost of item
+
+    // eventbrite
+    var eventbriteInviteImg = ("<img class='invite-img' src=" + inviteObject.eventbrite.dataImg + ">")
+    $("#eventbrite-display").append(eventbriteInviteImg)
+    eventbriteInviteDetails1 = ("<h5 class='invite-card-text'>" + inviteObject.eventbrite.dataName + "</h5>")
+    $("#eventbrite-display").append(eventbriteInviteDetails1)
+    // add link to event
+    // add time of event
+    // add location of event
+    // 
 };
 
 renderInvite();
